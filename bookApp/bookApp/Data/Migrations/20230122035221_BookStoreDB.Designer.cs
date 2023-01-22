aac5bc1647c141b46d98408f4703d706ed20afd4
nishaelango <123277427+nishaelango@users.noreply.github.com>
2023-01-22 14:57:42 +1100

    Added NuGet ConsoleTables and DBMigration scripts

--- bookApp/bookApp/Data/Migrations/20230122035221_BookStoreDB.Designer.cs
+++ bookApp/bookApp/Data/Migrations/20230122035221_BookStoreDB.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using bookApp.Data;
 
 namespace bookApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230122035221_BookStoreDB")]
+    partial class BookStoreDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ bookApp/bookApp/Data/Migrations/20230122035221_BookStoreDB.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace bookApp.Data.Migrations
+{
+    public partial class BookStoreDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BookId = table.Column<string>(nullable: false),
+                    BookName = table.Column<string>(nullable: false),
+                    Author = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
