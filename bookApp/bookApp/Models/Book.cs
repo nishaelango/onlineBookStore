08b993f4048a4a2431858ad9df9eae93357d7b92
nishaelango <123277427+nishaelango@users.noreply.github.com>
2023-01-22 14:31:23 +1100

    Model Checkin

---
+++ bookApp/bookApp/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace bookApp.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string BookId { get; set; }
+        [Required]
+        public string BookName { get; set; }
+        public string Author { get; set; }
+    }
+}
